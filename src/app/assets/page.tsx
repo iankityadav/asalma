ec28ee5d1b1bfcfcb3d6ac27794d7fedc1f6d9af
Ankit Yadav <64765952+iankityadav@users.noreply.github.com>
2024-09-28 17:00:16 +0530

    added asset page

---
+++ src/app/assets/page.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const AssetsPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-items-center gap-8">
+      <div className="flex flex-col p-4 mt-8 bg-white max-w-[720px] w-full rounded-md shadow-lg">
+        <div className="flex justify-between">
+          <div className="text-2xl font-light flex gap-3">
+            <div className="flex items-center text-blue-400">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M5.25 14.25h13.5m-13.5 0a3 3 0 0 1-3-3m3 3a3 3 0 1 0 0 6h13.5a3 3 0 1 0 0-6m-16.5-3a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3m-19.5 0a4.5 4.5 0 0 1 .9-2.7L5.737 5.1a3.375 3.375 0 0 1 2.7-1.35h7.126c1.062 0 2.062.5 2.7 1.35l2.587 3.45a4.5 4.5 0 0 1 .9 2.7m0 0a3 3 0 0 1-3 3m0 3h.008v.008h-.008v-.008Zm0-6h.008v.008h-.008v-.008Zm-3 6h.008v.008h-.008v-.008Zm0-6h.008v.008h-.008v-.008Z"
+                />
+              </svg>
+            </div>
+            <span className="flex items-center font-extralight">Assets</span>
+          </div>
+          <button className="border-2 border-blue-400 rounded-md px-3 py-1">
+            <div className="text-gray-600 flex gap-2">
+              <div className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z"
+                  />
+                </svg>
+              </div>
+              <div className="flex items-center">New Asset</div>
+            </div>
+          </button>
+        </div>
+      </div>
+      <div className="grid grid-cols-3 justify-center items-center p-4 bg-white max-w-[720px] w-full rounded-md shadow-lg gap-3">
+        <div className="w-full border-2 border-blue-200 rounded-md max-w-[220px] p-2">
+          <div className="font-semibold">Asset name</div>
+          <div className="text-sm">Asset Type</div>
+          <div className="text-sm">Assigned</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AssetsPage;
---
+++ src/app/employees/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EmployeePage = () => {
+  return (
+    <div className="flex flex-col items-center justify-items-center gap-8">
+      <div className="flex justify-center h-full mt-8">page</div>
+    </div>
+  );
+};
+
+export default EmployeePage;
